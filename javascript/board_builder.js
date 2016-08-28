2a068ecc2959de48be68c05cb8049764064a3320
Duncan Nevin <duncan.nevin@gmail.com>
2016-08-28 13:25:21 -0400

    Working product, will return true if the board has the correct entries. The program checks the board with every keyup

--- javascript/board_builder.js
+++ javascript/board_builder.js
@@ -1,59 +1,61 @@
-$(document).ready(function(){
-	//Wrap the board assembly in a function so it can be used in any format in the DOM...
-	var assembleBoard = function(){
-		//Write a function that creates the original boards layout based on a winning combonation, should also include
-		//all the form elements for input...		        
-		var createBoardPattern = function(){
-			//Create a variable with a winning board in order to populate the template
-			var winningBoard = [[5, 3, 4, 6, 7, 8, 9, 1, 2], 
-						        [6, 7, 2, 1, 9, 5, 3, 4, 8],
-						        [1, 9, 8, 3, 4, 2, 5, 6, 7],
-						        [8, 5, 9, 7, 6, 1, 4, 2, 3],
-						        [4, 2, 6, 8, 5, 3, 7, 9, 1],
-						        [7, 1, 3, 9, 2, 4, 8, 5, 6],
-						        [9, 6, 1, 5, 3, 7, 2, 8, 4],
-						        [2, 8, 7, 4, 1, 9, 6, 3, 5],
-						        [3, 4, 5, 2, 8, 6, 1, 7, 9]];			        
-			//Create a newBoard that will have between 1 and 5 correct numbers in each row...			        
-			var newBoard = _.map(winningBoard, (sub, ind)=>{
-				//Create an array with 0 - 8 random numbers between 0 - 9 that will be the random indexes...
-				var indexes = [];
-				while(indexes.length < Math.floor(Math.random() * 9)){
-					var randomNum = Math.floor(Math.random() * 10);
-					if(!_.some(indexes, (curr)=>{return randomNum === curr})){
-						indexes.push(randomNum);
-					}
-				}
-				return _.map(sub, (val, ind2, arr)=>{
-					if(_.some(indexes, (num)=>{return num === val})){
-						return val;
-					}else{
-						return 0;
-					}
-				});
-			});			        
-			return newBoard;
-		};			        
-		//Create a function that builds the board latout and appends it to the DOM...
-		var buildBoardLayout = function(pattern){
-			console.log(pattern)
-			//Use a for loop to create the board
-			var boxCounter = 1;			        			        
-			for(var i = 1; i < 10; i++){			
-				$('.gameBoard').append('<div class="row number'+ i +'"></div>');
-				for(var j = 1; j < 10; j++){				
-					$('.number' + i).append('<div class="square box'+ boxCounter +'"></div>');
-					if(i < 9 && i % 3 === 0){
-						$('.box' + boxCounter).css('border-bottom', '2px solid black');
-					}
-					if(j % 9 !== 0 && j % 3 === 0){
-						$('.box' + boxCounter).css('border-right', '2px solid black');
-					}
-					boxCounter++;
+//Wrap the board assembly in a function so it can be used in any format in the DOM...
+var assembleBoard = function(){
+	//Write a function that creates the original boards layout based on a winning combonation, should also include
+	//all the form elements for input...
+
+	var createBoardPattern = function(){
+		//Create a variable with a winning board in order to populate the template
+		var winningBoard = [[5, 3, 4, 6, 7, 8, 9, 1, 2], 
+					        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+					        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+					        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+					        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+					        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+					        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+					        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+					        [3, 4, 5, 2, 8, 6, 1, 7, 9]];	
+					        			        		        
+		//Create a newBoard that will have between 1 and 5 correct numbers in each row...			        
+		var newBoard = _.map(winningBoard, (sub, ind)=>{
+			//Create an array with 0 - 8 random numbers between 0 - 9 that will be the random indexes...
+			var indexes = [];
+			while(indexes.length < Math.floor(Math.random() * 9)){
+				var randomNum = Math.floor(Math.random() * 10);
+				if(!_.some(indexes, (curr)=>{return randomNum === curr})){
+					indexes.push(randomNum);
 				}
 			}
-		}	
-		buildBoardLayout(createBoardPattern());				        
-	};
-	assembleBoard();
-});
+			return _.map(sub, (val, ind2, arr)=>{
+				if(_.some(indexes, (num)=>{return num === val})){
+					//adds these values to the current game board...
+					disBoard(ind, ind2, val);
+					return val;
+				}else{
+					return '<input id="dis' + ind + ind2 + '" class="userEntry" type="text" maxlength="1" onkeypress="return isNumber1To9(event)" onkeyup="getSelectionInfo(' + ind + ', ' + ind2 + ')"></input>';
+				}
+			});
+		});		        
+		return newBoard;
+	};			        
+	//Create a function that builds the board latout and appends it to the DOM...
+	var buildBoardLayout = function(pattern){
+		//Use a nested for loop to create the board
+		var boxCounter = 1;			        			        
+		for(var i = 1; i < 10; i++){			
+			$('.gameBoard').append('<div class="row number'+ i +'"></div>');
+			for(var j = 1; j < 10; j++){				
+				$('.number' + i).append('<div class="square box'+ boxCounter +'">' + pattern[i-1][j-1] + '</div>');
+				if(i < 9 && i % 3 === 0){
+					$('.box' + boxCounter).css('border-bottom', '2px solid black');
+				}
+				if(j % 9 !== 0 && j % 3 === 0){
+					$('.box' + boxCounter).css('border-right', '2px solid black');
+				}
+				boxCounter++;
+			}
+		}
+	}	
+	buildBoardLayout(createBoardPattern());				        
+};
+
+
---
+++ javascript/control.js
@@ -0,0 +1,41 @@
+//Variable storing gameboard, use closure in order to store the gameboard with updated values...
+var liveGameBoard = function(){
+	var board = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+				 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+				 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+				 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+				 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+				 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+				 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+				 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+				 [0, 0, 0, 0, 0, 0, 0, 0, 0]];
+	
+	return function updateBoard(ind1, ind2, val){
+		board[ind1][ind2] = val;
+		validSolution(board);
+	};		 
+};
+var disBoard = liveGameBoard();
+//Function that will only allow valid entries 1-9
+var isNumber1To9 = function(evt){
+	var charCode = (evt.which) ? evt.which : evt.keyCode;
+	if(charCode > 31 && (charCode < 48 || charCode > 57)){
+		return false;
+	}else{
+		return true;
+	}
+}
+//Function to gather the entered value...
+var whatIsEntered = function(loc){
+	return loc.value;
+};
+//When a key is pressed get the location of the entry and the value, then uses closer with livegameboard to store the value...
+var getSelectionInfo = function(ind1, ind2){
+	var getTheDigit = Number(whatIsEntered(document.getElementById('dis' + ind1 + ind2 + '')));
+	console.log([ind1, ind2, getTheDigit]);
+	disBoard(ind1, ind2, getTheDigit);
+};
+//Load board on pageload...
+$(document).ready(function(){
+	assembleBoard();
+});
